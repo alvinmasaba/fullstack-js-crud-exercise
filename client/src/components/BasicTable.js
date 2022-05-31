ab24be9d4036f62466fd0e86244be7364b2fbeee
alvinmasaba <alvinmasaba@gmail.com>
2022-05-31 02:36:17 -0400

    Adds colors to table.

--- client/src/components/BasicTable.js
+++ client/src/components/BasicTable.js
@@ -22,43 +22,61 @@ export const BasicTable = () => {
     prepareRow,
   } = tableInstance
 
-  // Displays red or green dot in home table based on active state.
-  function AssignedState(cell) {
+  // Handles styling for color and assigned state.
+
+  function CheckCellValue(cell) {
     if (cell.column.Header === 'Assigned') {
-      if (cell.value === true) {
+      return AssignedState(cell.value)
+    } else if (cell.column.Header === 'Color') {
+      return DisplayColor(cell.value)
+    } else {
+      return cell.render('Cell')
+    };
+  };
+
+  // Displays employee color.
+  function DisplayColor(color) {
+    if (color === "") {
+      return "";
+    }
+    return <span className="color-circle" style={{backgroundColor: color, boxShadow: `0px 0px 2px 1px ${color}`}}></span>;
+  };
+
+  // Displays red or green dot in home table based on active state.
+  function AssignedState(value) {
+      if (value === true || value === 'true') {
         return <span className="dot" id="green"></span>;
       } else {
         return <span className="dot" id="red"></span>;
       }
-    } else {
-      return cell.render('Cell')
-    }
-  };
+    };
 
   return (
-    <table {...getTableProps()} className="styled-table">
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map( column => (
-                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{AssignedState(cell)}</td>
-              })}
+    <div style={{marginTop: "150px"}}>
+      <table {...getTableProps()} className="styled-table">
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map( column => (
+                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{CheckCellValue(cell)}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 };
 
